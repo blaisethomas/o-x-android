4004834583847d4f7d52ad412867bf59f21a3430
Blaise Thomas <blaisegthomas@gmail.com>
2016-04-22 15:05:59 -0700

    main activity calls game activity and passes player names

--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,13 +1,35 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        TextView game_message_text = (TextView) findViewById(R.id.game_message_text);
+
+
+        Intent intent = getIntent();
+        String player1 = intent.getStringExtra("player1");
+        String player2 = intent.getStringExtra("player2");
+
+        game_message_text.setText(player1 + "'s turn");
+
+
+
     }
+
+
+
+
+
 }
--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -1,11 +1,11 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button startPlaying = (Button) findViewById(R.id.start_game_button);
+        final TextView player1name = (TextView)findViewById(R.id.player_one_name);
+        final TextView player2name = (TextView)findViewById(R.id.player_two_name);
+
+
+        startPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String player1 = player1name.getText().toString();
+                String player2 = player2name.getText().toString();
+
+                Intent startGame = new Intent(MainActivity.this, GameActivity.class);
+                startGame.putExtra("player1", player1);
+                startGame.putExtra("player2", player2);
+                startActivity(startGame);
+            }
+        });
     }
+
 }
