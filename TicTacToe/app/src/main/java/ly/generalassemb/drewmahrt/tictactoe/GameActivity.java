c1c7784004faf03c3a45b305267ab86d856ede36
Blaise Thomas <blaisegthomas@gmail.com>
2016-04-22 16:15:58 -0700

    refactor var declarations/ assignments

--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -16,6 +16,20 @@ public class GameActivity extends AppCompatActivity {
     ArrayList<Integer> player1moves = new ArrayList();
     ArrayList<Integer> player2moves = new ArrayList();
     int counter;
+    String player1;
+    String player2;
+    TextView game_message_text;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+    TextView textView8;
+    TextView textView9;
+
+
 
 
     @Override
@@ -23,20 +37,20 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        final TextView game_message_text = (TextView) findViewById(R.id.game_message_text);
-        final TextView textView1 = (TextView)findViewById(R.id.textView1);
-        final TextView textView2 = (TextView)findViewById(R.id.textView2);
-        final TextView textView3 = (TextView)findViewById(R.id.textView3);
-        final TextView textView4 = (TextView)findViewById(R.id.textView4);
-        final TextView textView5 = (TextView)findViewById(R.id.textView5);
-        final TextView textView6 = (TextView)findViewById(R.id.textView6);
-        final TextView textView7 = (TextView)findViewById(R.id.textView7);
-        final TextView textView8 = (TextView)findViewById(R.id.textView8);
-        final TextView textView9 = (TextView)findViewById(R.id.textView9);
+        game_message_text = (TextView) findViewById(R.id.game_message_text);
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView3 = (TextView)findViewById(R.id.textView3);
+        textView4 = (TextView)findViewById(R.id.textView4);
+        textView5 = (TextView)findViewById(R.id.textView5);
+        textView6 = (TextView)findViewById(R.id.textView6);
+        textView7 = (TextView)findViewById(R.id.textView7);
+        textView8 = (TextView)findViewById(R.id.textView8);
+        textView9 = (TextView)findViewById(R.id.textView9);
 
         Intent intent = getIntent();
-        final String player1 = intent.getStringExtra("player1");
-        final String player2 = intent.getStringExtra("player2");
+        player1 = intent.getStringExtra("player1");
+        player2 = intent.getStringExtra("player2");
         game_message_text.setText(player1 + "'s turn");
 
 
@@ -63,120 +77,173 @@ public class GameActivity extends AppCompatActivity {
         textView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView2.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView2.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView2.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView2.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(2);
+                    } else if (counter % 2 != 0) {
+                        textView2.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(2);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView3.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView3.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView3.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView3.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(3);
+                    } else if (counter % 2 != 0) {
+                        textView3.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(3);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView4.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView4.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView4.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView4.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(4);
+                    } else if (counter % 2 != 0) {
+                        textView4.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(4);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView5.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView5.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView5.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView5.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(5);
+                    } else if (counter % 2 != 0) {
+                        textView5.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(5);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView6.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView6.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView6.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView6.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(6);
+                    } else if (counter % 2 != 0) {
+                        textView6.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(6);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView7.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView7.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView7.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView7.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(7);
+                    } else if (counter % 2 != 0) {
+                        textView7.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(7);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView8.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView8.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView8.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView8.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(8);
+                    } else if (counter % 2 != 0) {
+                        textView8.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(8);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
 
         textView9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (counter%2 == 0){
-                    textView9.setText("x");
-                    game_message_text.setText(player2 + "'s turn");
-                } else if (counter %2 != 0){
-                    textView9.setText("o");
-                    game_message_text.setText(player1+ "'s turn");
+                if (textView9.getText().toString() == "" ) {
+                    if (counter % 2 == 0) {
+                        textView9.setText("x");
+                        game_message_text.setText(player2 + "'s turn");
+                        player1moves.add(9);
+                    } else if (counter % 2 != 0) {
+                        textView9.setText("o");
+                        game_message_text.setText(player1 + "'s turn");
+                        player2moves.add(9);
+                    }
+                    winCheck();
+                } else {
+                    Toast.makeText(GameActivity.this, "Seat's Taken", Toast.LENGTH_LONG).show();
                 }
-                winCheck();
             }
         });
-
 
     }// end of on create
 
     public boolean winCheck(){
         Log.d(TAG, "win check invoked" );
+
+        if (player1moves.contains(1) && player1moves.contains(2) && player1moves.contains(3)){
+            Toast.makeText(GameActivity.this, "X wins", Toast.LENGTH_SHORT).show();
+        }
+
+
         counter ++;
         return true;
     }
